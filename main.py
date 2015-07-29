9cebdca64b328259e523f245eecae527f4fd0efa
Rory Crispin <me@rorycrispin.co.uk>
2015-07-29 11:51:38 +0100

    Breathe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import rbgLib
 import time
+
 __author__ = 'Rory'
 rbgLib.init()
 redLED = rbgLib.LED(18, True, 200)
@@ -7,10 +8,13 @@ greenLED = rbgLib.LED(24, True, 200)
 blueLED = rbgLib.LED(23, True, 200)
 
 rgbLEDx = rbgLib.rbgLed(redLED, greenLED, blueLED)
-rgbLEDx.set_colour((rbgLib.rgbColour(100, 10, 10)))
-time.sleep(1)
-rbgLib.ledAlert(rbgLib.rgbColour(10, 100, 10), rgbLEDx, 50)
-time.sleep(1)
-rbgLib.ledAlert(rbgLib.orange,rgbLEDx,100)
-rbgLib.ledAlert(rbgLib.violet,rgbLEDx,100)
+#rgbLEDx.set_colour((rbgLib.rgbColour(100, 10, 10)))
+#rbgLib.ledAlert(rbgLib.rgbColour(10, 100, 10), rgbLEDx, 50)
+#rbgLib.ledAlert(rbgLib.orange, rgbLEDx, 100)
+#rgbLEDx.fade_to(rbgLib.rgbColour(0, 0, 0), 100)
+
+pass
+rbgLib.mode_breathe(False,rgbLEDx,rbgLib.yellow,100).start()
+
+
 rbgLib.exit(rgbLEDx)
--- rbgLib.py
+++ rbgLib.py
@@ -9,26 +9,43 @@ class rgbColour(object):
     green = 0
     blue = 0
 
-    def __init__(self, red, green, blue):
-        self.red = red
-        self.green = green
-        self.blue = blue
+    def __init__(self, red, green, blue, safe_brightness=False):
+        # By default safe_brightness will block low duty cycles to keep fade motions smooth
+        if not safe_brightness:
+            self.red = red
+            self.green = green
+            self.blue = blue
+        else:
+            self.safe_brightnessF(red, green, blue, safe_brightness)
 
-def hexToColour(r,g,b):
+    def with_brightness(self, multiplier):
+        return rgbColour(self.red * multiplier, self.green * multiplier, self.blue * multiplier)
+
+    def safe_brightnessF(self, red, green, blue, safe_b):
+        def set_min(val, minimum):
+            if val < minimum:
+                return minimum
+            else:
+                return val
+        self.red = set_min(red, safe_b)
+        self.blue = set_min(blue, safe_b)
+        self.green = set_min(green, safe_b)
+
+
+def hexToColour(r, g, b):
     hex_constant = 0.3921568627
-    return rgbColour(hex_constant*r, hex_constant*g, hex_constant*b)
-
-red = rgbColour(100,0,0)
-green = rgbColour(0,100,0)
-blue = rgbColour(0,0,100)
-orange  = hexToColour(255, 127, 0)
-yellow  = hexToColour(255,255,0)
-indigo  = hexToColour(75,0,130)
-purple  = rgbColour(100,0,100)
-aqua = rgbColour(0,100,100)
-turquoise = rgbColour(0,100,30)
+    return rgbColour(hex_constant * r, hex_constant * g, hex_constant * b)
 
 
+red = rgbColour(100, 0, 0)
+green = rgbColour(0, 100, 0)
+blue = rgbColour(0, 0, 100)
+orange = hexToColour(255, 127, 0)
+yellow = hexToColour(255, 255, 0)
+indigo = hexToColour(75, 0, 130)
+purple = rgbColour(100, 0, 100)
+aqua = rgbColour(0, 100, 100)
+turquoise = rgbColour(0, 100, 30)
 
 
 # noinspection PyPep8Naming
@@ -65,7 +82,6 @@ class rbgLed(object):
     def fade_to(self, destColour, length):
         startColour = self.get_colour()
         redDelta = find_delta(startColour.red, destColour.red, length)
-        print("RedDelta = " + str(redDelta))
         greenDelta = find_delta(startColour.green, destColour.green, length)
         blueDelta = find_delta(startColour.blue, destColour.blue, length)
         for i in range(0, length + 1, 1):
@@ -74,7 +90,56 @@ class rbgLed(object):
                            startColour.green + (i * greenDelta),
                            startColour.blue + (i * blueDelta))
             self.set_colour(to)
-            print to.red
+
+
+class RGBFunctionParent(object):
+    interrupt = False
+    func_name = None
+    run_time = 0
+    run_infinite = False
+    rgbled = None
+
+    def __init__(self, func_name, run_time, rgbled):
+        self.func_name = func_name
+        self.rgbled = rgbled
+        # Set run_time to False to run indefinitely
+        if not run_time:
+            self.run_infinite = True
+        else:
+            self.run_time = run_time
+
+    def start(self):
+        self.main_func()
+
+    def main_func(self):
+        pass
+
+    def pause(self):
+        pass
+
+    def main_func(self):
+        pass
+
+    def interrupt_func(self):
+        self.interrupt = True
+        self.pause()
+
+
+class mode_breathe(RGBFunctionParent):
+    colour = None
+    pulse_time = 0
+
+    def __init__(self, run_time, rgbLed, colour, pulse_time):
+        super(mode_breathe, self).__init__("breathe", run_time, rgbLed)
+        self.colour = colour
+        self.pulse_time = pulse_time
+
+    def main_func(self):
+        while 1:
+            if self.interrupt:
+                break
+            self.rgbled.fade_to(self.colour, int(self.pulse_time / 2))
+            self.rgbled.fade_to(yellow.with_brightness(0.2), int(self.pulse_time / 2))
 
 
 class LED(object):
@@ -108,6 +173,7 @@ def ledAlert(destColour, rgbLEDy, length):
 def init():
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
+
 
 def exit(LED):
     LED.turn_off()
